a053fcf664f89a24a0f6317760104c907131c829
menashe <immmalessa@gmail.com>
2023-01-08 14:10:17 +0200

    m

---
+++ 1.1_primitive_data_types.js
@@ -0,0 +1,15 @@
+var yep="yep";
+var num= 6;
+var obj=[1,3,4]
+var boole= true;
+var nul =null;
+var un = "hii"[5] ;
+let x=_;
+
+console.log(typeof(str))
+console.log(typeof(num))
+console.log(typeof(obj))
+console.log(typeof(boole))
+console.log(typeof(nul))
+console.log(typeof(un))
+console.log(typeof(x))
