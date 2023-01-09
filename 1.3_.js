4a8cfe30cdd93886f266d7f0d159fd12f49235fa
menashe <immmalessa@gmail.com>
2023-01-09 10:19:54 +0200

    m

---
+++ 1.3_.js
@@ -0,0 +1,1 @@
+//mdn date.prototype.getday()
---
+++ 2.1_functions.js
@@ -0,0 +1,15 @@
+// lines (9 sloc)  402 Bytes;
+
+function countryInfo (country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`
+}
+
+let exampleOne = countryInfo('France',67.75, 'Paris')
+
+let exampleTwo = countryInfo('Israel',9.364, 'Jerusalem')
+
+let exampleThree = countryInfo('Greece',10.64, 'Athens')
+
+console.log(exampleOne)
+console.log(exampleTwo)
+console.log(exampleThree)
