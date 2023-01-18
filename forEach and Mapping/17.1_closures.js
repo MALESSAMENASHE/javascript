9c180e96d863b5aef2e5f50da79c2a67aa8c9f6d
menashe <immmalessa@gmail.com>
2023-01-18 15:55:32 +0200

    m

--- forEach and Mapping/17.1_closures.js
+++ forEach and Mapping/17.1_closures.js
@@ -22,27 +22,27 @@
 
 // Block 2
 
-var a = 1;
-function b2() {
-a = 10;
-return;
-function a() { }
-}
-b2();
-console.log(a);
+// var a = 1;
+// function b2() {
+// a = 10;
+// return;
+// function a() { }
+// }
+// b2();
+// console.log(a);
 
 //results: 1
 //why..: we gets the global a, the hader a is local .
 
 // Block 3
 
-// let i;
-// for ( i = 0; i < 3; i++) {
-// const log = () => {
-// console.log(i);
-// }
-// setTimeout(log, 100);
-// }
+ let i;
+for ( i = 0; i < 3; i++) {
+const log = () => {
+console.log(i);
+}
+setTimeout(log, 1000);
+}
 
 //results: 3 3 3 
 //why..: print in delay, but i keep changing .then we get print of the consolle.log(!)  
